efcd2521c44b05785b64d4b7b94fba74282fe3d1
coneeone <nicolas.laudinet@sfr.fr>
2020-11-18 12:09:02 +0100

    doc update

--- src/controllers/message.js
+++ src/controllers/message.js
@@ -6,10 +6,10 @@ module.exports = {
      * Update un document "sujet" en ajoutant un message au tableau "reponses"
      * Renvoie le tableau de messages mis à jour
      * 
-     * @param req {Object} - Un objet requête d'express
-     * @param req.params.sujetID {String} - L'id du sujet à mettre à jour
-     * @param req.body {Object} - L'objet à ajouter dans le tableau "reponses"
-     * @param res {Object} - Un objet reponse d'express
+     * @param {Object} req - Un objet requête d'express
+     * @param {String} req.params.sujetID - L'id du sujet à mettre à jour
+     * @param {Object} req.body - L'objet à ajouter dans le tableau "reponses"
+     * @param {Object} res - Un objet reponse d'express
      * 
      */
     create: async (req, res) => {
