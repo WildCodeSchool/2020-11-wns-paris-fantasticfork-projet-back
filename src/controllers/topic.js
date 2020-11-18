2cec41cfc8947aaaf8dae0ca405b58603a1aa1f6
coneeone <nicolas.laudinet@sfr.fr>
2020-11-18 22:40:01 +0100

    read & readOne returns comments

--- src/controllers/topic.js
+++ src/controllers/topic.js
@@ -1,5 +1,6 @@
 const TopicModel = require("../models/Topic");
 const UserModel = require("../models/User")
+const CommentModel = require('../models/Comment')
 
 module.exports = {
     /**
@@ -15,7 +16,6 @@ module.exports = {
 
         try {
             req.body.date = new Date(Date.now()) // sets date of topic
-
             const topic = new TopicModel(req.body);
             const result = await topic.save();
             res.json({ success: true, body: result }); 
@@ -35,13 +35,25 @@ module.exports = {
      */
     read: async (req, res) => {
         try {
-            const result = await TopicModel.find({})
+            const topics = await TopicModel.find({})
             .populate('author')
             .populate('tags')
-            /* uncomment the line below to populate 'comments' in the response */
-            // .populate('comments')
+            
+            const comments = await CommentModel.find({})
+            .populate('author')
 
-            res.json({ success: true, body: result });
+            comments.forEach( (comment) => {
+                console.log(comment)
+                console.log("COMMENTS : ", comment.topicID)
+            })
+
+            topics.forEach( (topic) => {
+                console.log("TOPICS : ", topic._id)
+            })
+
+            let topicsWithComments = nestCommentsInTopics(topics, comments)
+
+            res.json({ success: true, body: topicsWithComments });
         } 
         catch (error) {
             console.log("err: ", error);
@@ -61,12 +73,16 @@ module.exports = {
         await TopicModel.init(); // ?
 
         try {
-            const result = await TopicModel.findOne({ _id: req.params.id })
+            const topics = await TopicModel.find({ _id: req.params.id })
             .populate('author')
             .populate('tags')
-            .populate('comments');
 
-            res.json({ success: true, body: result });
+            const comments = await CommentModel.find({ topicID: req.params.id })
+            .populate('author')
+
+            const nestedArrays = nestCommentsInTopics(topics, comments)
+
+            res.json({ success: true, body: nestedArrays });
         } 
         catch (error) {
             console.log("err: ", error);
@@ -74,3 +90,16 @@ module.exports = {
         }
     },
 };
+
+function nestCommentsInTopics(topics, comments) {
+    let nestedArrays = []
+    topics.forEach( (topic) => {
+        comments.forEach( (comment) => {
+            if (topic._id.equals(comment.topicID)) {
+                topic.comments.push(comment)
+            }
+        })
+        nestedArrays.push(topic)
+    })
+    return nestedArrays
+}
