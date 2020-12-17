70fd2817e65472c485ef938c2090e9d3bc4e267c
coneeone <nicolas.laudinet@sfr.fr>
2020-12-18 00:36:25 +0100

    gql queries for one topic and topics

--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,9 @@ const express = require('express');
 const cors = require('cors');
 const route = require('./routes/index');
 
+const { graphqlHTTP } = require('express-graphql');
+const { buildSchema } = require('graphql');
+
 // init app
 const app = express();
 
@@ -13,5 +16,68 @@ app.use(cors());
 
 // Routes
 app.use(route);
+
+//GRAPHQL
+//schemas
+var schema = buildSchema(`
+    type Query {
+        topics: [Topic]
+        topic(_id: ID): Topic
+    },
+    type Topic {
+        _id: ID
+        username: String
+        subject: String
+        body: String
+        date: String
+        url: [String]
+        tags: [String]
+        comments: [String]
+        responses: [Response]
+    },
+    type Response {
+        date: String
+        name: String
+        message: String
+    },
+    
+`);
+
+//resolvers
+const TopicModel = require('./models/Topic')
+const CommentModel = require('./models/Comment')
+
+const getTopics = async function() {
+    try {
+        const topics = await TopicModel.find({})
+        return topics;
+    }
+
+    catch (error) {
+        console.log('err: ', error);
+        return error
+    }
+}
+
+const getOneTopic = async function (args : Args) {
+    const topic = await TopicModel.findById(args._id);
+    return topic;
+}
+
+interface Args {
+    _id: String,
+}
+
+const root = {
+    topics: getTopics,
+    topic: getOneTopic
+};
+
+app.use('/graphql', graphqlHTTP({
+    schema: schema,
+    rootValue: root,
+    graphiql: true
+}));
+
 
 module.exports = app;
