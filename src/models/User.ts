01036d240687bafe8e2f19ce34c930c2dc01e4a6
lenasim <radical.rin@gmail.com>
2021-02-03 13:51:32 +0100

    Add user model, type

--- src/models/User.ts
+++ src/models/User.ts
@@ -1,11 +1,53 @@
 import mongoose from 'mongoose';
 
-const UserSchema = new mongoose.Schema({
-  name: {
-    type: mongoose.SchemaTypes.String,
-    required: true,
-  },
-  created_at: mongoose.SchemaTypes.Date,
-});
+export interface IUser extends mongoose.Document {
+  email: string;
+  password: string;
+  firstname: string;
+  lastname: string;
+  tags?: [string];
+  role?: userRole;
+  createdAt?: Date;
+}
 
-export default mongoose.model('User', UserSchema);
+export enum userRole {
+  admin = 'ADMIN',
+  student = 'STUDENT',
+  teacher = 'TEACHER',
+}
+
+const UserModel = new mongoose.Schema(
+  {
+    email: {
+      type: mongoose.SchemaTypes.String,
+      required: true,
+      unique: true,
+      match: /.+@.+\..+/,
+    },
+    password: {
+      type: mongoose.SchemaTypes.String,
+      required: true,
+    },
+    firstname: {
+      type: mongoose.SchemaTypes.String,
+      required: true,
+      trim: true,
+    },
+    lastname: {
+      type: mongoose.SchemaTypes.String,
+      required: true,
+      trim: true,
+    },
+    tags: [mongoose.SchemaTypes.String],
+    role: {
+      type: mongoose.SchemaTypes.String,
+      enum: Object.values(userRole),
+      default: userRole.student,
+    },
+  },
+  {
+    timestamps: { createdAt: 'createdAt' },
+  }
+);
+
+export default mongoose.model('User', UserModel);
