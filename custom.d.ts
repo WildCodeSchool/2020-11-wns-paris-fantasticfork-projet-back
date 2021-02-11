df5e063aa6c5ae06c0519d0a95a40a0277e7befe
coneeone <nicolas.laudinet@sfr.fr>
2021-02-12 00:01:22 +0100

    typescript support for .env variables

--- custom.d.ts
+++ custom.d.ts
@@ -2,5 +2,16 @@ declare namespace Express {
   export interface Request {
     isAuth?: boolean;
     userID?: string;
+    Context?: Request;
+  }
+}
+
+declare namespace NodeJS {
+  export interface ProcessEnv {
+    NODE_ENV: string;
+    PORT: string;
+    MONGODB_URI: string;
+    JWT_SECRET: string;
+    JWT_LIFE_TIME: string;
   }
 }
