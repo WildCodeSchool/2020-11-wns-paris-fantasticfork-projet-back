ac5138e02e18445a95406c7a6c6fdb2dd6020548
coneeone <nicolas.laudinet@sfr.fr>
2021-03-03 16:44:30 +0100

    setup subscription endpoint

--- src/server.ts
+++ src/server.ts
@@ -14,6 +14,9 @@ mongooseConnect();
 
 // Apollo server
 const server = new ApolloServer({
+  subscriptions: {
+    path: '/graphql',
+  },
   typeDefs,
   resolvers,
   introspection: true,
@@ -28,4 +31,6 @@ server.applyMiddleware({ app });
 
 // eslint-disable-next-line no-console
 const port = process.env.PORT || 4000;
-app.listen(port, () => console.log(`Server started on ${port}`));
+app.listen(port, () =>
+  console.log(`Server started on ${port}`, server.subscriptionsPath)
+);
