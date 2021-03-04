40c5e4678ddc28145c6def9221d650cbf5263e74
coneeone <nicolas.laudinet@sfr.fr>
2021-03-04 11:51:32 +0100

    saves messages to db

--- src/resolvers/chat.ts
+++ src/resolvers/chat.ts
@@ -1,3 +1,4 @@
+import MessageModel from '../models/Message';
 import { PubSub } from 'apollo-server-express';
 const pubsub = new PubSub();
 
@@ -10,17 +11,11 @@ export const chatSubscription = {
 };
 
 export const chatMutation = {
-  newMessage: (
+  newMessage: async (
     _: unknown,
     { text, userId, username }: IMessageInput
-  ): IMessageOutput => {
-    const date = new Date();
-    const message = {
-      text,
-      userId,
-      username,
-      date,
-    };
+  ): Promise<IMessageOutput> => {
+    const message = await new MessageModel({ text, userId, username }).save();
     pubsub.publish('NEW_MESSAGE', { chatFeed: message });
     return message;
   },
@@ -33,5 +28,5 @@ interface IMessageInput {
 }
 
 interface IMessageOutput extends IMessageInput {
-  date: Date;
+  createdAt: Date;
 }
