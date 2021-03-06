82cbd4fb9aef076f7d02a219456b28f2d07a8d7d
lenasim <radical.rin@gmail.com>
2021-03-06 23:01:38 +0100

    add express response in return value of context

--- src/middlewares/authenticateRequest.ts
+++ src/middlewares/authenticateRequest.ts
@@ -1,45 +1,48 @@
-import { Request } from 'express';
+import { Response, Request } from 'express';
 import jwt from 'jsonwebtoken';
 import UserModel from '../models/User';
 
 export default async ({
+  res,
   req,
 }: {
+  res: Response;
   req: Request;
 }): Promise<AuthContextReturn> => {
   // when using websocket subscriptions, req is unset
-  if (!req) return { isAuth: false };
+  if (!req) return { res, isAuth: false };
 
   const authHeader = req.get('Authorization');
   if (!authHeader) {
-    return { isAuth: false };
+    return { res, isAuth: false };
   }
 
   const token = authHeader.split(' ')[1];
   if (!token || token === '') {
-    return { isAuth: false };
+    return { res, isAuth: false };
   }
 
-  let decodedToken;
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  let decodedToken: any;
   try {
-    // was not able to find a way to give a type to the return of the jwt.verify() function
-    decodedToken = <any>jwt.verify(token, process.env.JWT_SECRET);
+    decodedToken = jwt.verify(token, process.env.JWT_SECRET);
   } catch (err) {
-    return { isAuth: false };
+    return { res, isAuth: false };
   }
 
   if (!decodedToken) {
-    return { isAuth: false };
+    return { res, isAuth: false };
   }
 
   await UserModel.findByIdAndUpdate(decodedToken.userID, {
     $set: { lastActivity: Date.now() },
   });
 
-  return { isAuth: true, userID: decodedToken.userID };
+  return { res, isAuth: true, userID: decodedToken.userID };
 };
 
 interface AuthContextReturn {
+  res: Response;
   isAuth: boolean;
   userID?: string;
 }
