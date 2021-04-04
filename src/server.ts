5c28eeae62b1015d371e0456bee67564f91a8786
lenasim <radical.rin@gmail.com>
2021-04-04 22:45:28 +0200

    CORS change url

--- src/server.ts
+++ src/server.ts
@@ -30,7 +30,7 @@ const server = new ApolloServer({
 const app = express();
 app.use(
   cors({
-    origin: 'https://stud-connect.netlify.app/',
+    origin: 'https://stud-connect.netlify.app',
     // process.env.NODE_ENV === 'dev'
     //   ? 'http://localhost:3000'
     //   : 'https://stud-connect.netlify.app/',
