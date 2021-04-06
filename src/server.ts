c7509a02201360fe8d43b6d1223ca5472aa4ed1e
Nicolas Laudinet <65535184+coneeone@users.noreply.github.com>
2021-04-06 14:44:24 +0200

    Set cors origin to an array of values (netlify + localhost)

--- src/server.ts
+++ src/server.ts
@@ -30,10 +30,7 @@ const server = new ApolloServer({
 const app = express();
 app.use(
   cors({
-    origin:
-      process.env.NODE_ENV === 'dev'
-        ? 'http://localhost:3000'
-        : 'https://stud-connect.netlify.app',
+    origin: ['http://localhost:3000', 'https://stud-connect.netlify.app'],
     credentials: true,
   })
 );
