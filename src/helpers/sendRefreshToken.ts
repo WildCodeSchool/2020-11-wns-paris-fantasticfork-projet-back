b2a69e892ed7cd5e4e030e862bf675c6afb5b594
lenasim <radical.rin@gmail.com>
2021-05-03 14:45:37 +0200

    Make cookie persistent

--- src/helpers/sendRefreshToken.ts
+++ src/helpers/sendRefreshToken.ts
@@ -2,6 +2,7 @@ import { Response } from 'express';
 
 const sendRefreshToken = (res: Response, refreshToken: string): Response => {
   return res.cookie('jid', refreshToken, {
+    maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
     httpOnly: true,
     path: '/refresh_token',
   });
