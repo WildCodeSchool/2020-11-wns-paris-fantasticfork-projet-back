f2b9d0a7e1eaa8394a47dce4bcd15b3fd8d58466
lenasim <radical.rin@gmail.com>
2021-05-05 11:21:52 +0200

    add cors list

--- src/server.ts
+++ src/server.ts
@@ -30,7 +30,11 @@ const server = new ApolloServer({
 const app = express();
 app.use(
   cors({
-    origin: ['http://localhost:3000', 'https://stud-connect.netlify.app'],
+    origin: [
+      'http://localhost:3000',
+      'https://stud-connect.netlify.app',
+      'https://studconnect.wns.wilders.dev',
+    ],
     credentials: true,
   })
 );
