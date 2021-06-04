b7dd87a28ae6c6498e052eb5a2f32c81ea394ac3
lenasim <radical.rin@gmail.com>
2021-06-04 11:23:50 +0200

    Add cookie secure option

--- src/helpers/sendRefreshToken.ts
+++ src/helpers/sendRefreshToken.ts
@@ -4,6 +4,7 @@ const sendRefreshToken = (res: Response, refreshToken: string): Response => {
   return res.cookie('jid', refreshToken, {
     maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
     httpOnly: true,
+    secure: true,
     path: '/refresh_token',
   });
 };
