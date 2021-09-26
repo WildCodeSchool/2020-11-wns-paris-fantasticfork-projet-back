b48b6e565d722457c9d99d3858e34c6fe0da4340
coneeone <nicolas.laudinet@sfr.fr>
2021-09-26 15:38:01 +0200

    stores user id of the comment author

--- src/models/Comment.ts
+++ src/models/Comment.ts
@@ -23,11 +23,11 @@ export interface ICommentUpdates extends mongoose.Document {
 
 const CommentModel = new mongoose.Schema(
   {
-    // authorId: {
-    //   type: mongoose.Schema.Types.ObjectId,
-    //   ref: 'User',
-    //   required: true,
-    // },
+    authorID: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User',
+      required: true,
+    },
     topicId: {
       type: mongoose.Schema.Types.ObjectId,
       ref: 'Topic',
